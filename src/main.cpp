1131eeedb89012fb8109fc274ed46765114c589b
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-09-20 13:24:57 -0600

    receive input from user

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,18 @@
+#include <iostream>
+#include <string>
+
+void printPrompt()
+{
+  std::cout << "shell379> " << std::flush;
+}
+
 int main(int argc, char *argv[])
 {
+  while (true)
+  {
+    printPrompt();
+    std::string input;
+    std::cin >> input;
+    std::cout << input << std::endl;
+  }
 }
