c34f98f6cc751dddc2ce35f19ffeed658497362b
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2021-09-26 02:59:52 -0600

    transform input into a class to get default values

--- src/InputOptions.hpp
+++ src/InputOptions.hpp
@@ -4,10 +4,12 @@
 
 #define MAX_ARGS 7 // Max number of arguments to a command
 
-typedef struct
+class InputOptions
 {
+public:
+  InputOptions() : inBackground(false) {}
   bool inBackground;
   std::vector<std::string> cmdArgs;
   std::vector<std::string> inputFiles;
   std::vector<std::string> outputFiles;
-} InputOptions;
+};
